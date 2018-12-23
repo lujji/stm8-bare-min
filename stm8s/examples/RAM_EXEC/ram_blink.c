258ae9ef24fd8eb6fc86d9667cd4a0b842f8d0fb
Georg Icking-Konert <icking@onlinehome.de>
2018-12-23 12:25:28 +0100

    simplify GPIO selection
    simplify GPIO selection (set in ram.h)

--- stm8s/examples/RAM_EXEC/ram_blink.c
+++ stm8s/examples/RAM_EXEC/ram_blink.c
@@ -4,8 +4,8 @@
 #pragma codeseg RAM0
 void ram_blink(uint8_t count) {
     while (count--) {
-        PD_ODR ^= (1 << 4);
-        for (uint32_t i = 0; i < 0xFFF; i++)
-            __asm__("nop");
+        LED_ODR ^= (1 << LED_PIN);
+        for (uint32_t i = 0; i < 0xFFFF; i++)
+            nop();
     }
 }
